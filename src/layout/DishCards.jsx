8d721dc87e6f4173b32d2d478f982494183d0e33
SundarHari003 <sundarhari014@gmail.com>
2024-03-13 12:49:31 +0530

    card with change

--- src/layout/DishCards.jsx
+++ src/layout/DishCards.jsx
@@ -3,7 +3,7 @@ import  {BsStarFill, BsStarHalf} from 'react-icons/bs'
 import Buttons from './Buttons'
 const DishCards = (props) => {
   return (
-    <div className=' w-4/6 md:w-3/6 lg:w-1/4 p-5 shadow-[rgba(0,0,0,0.2)_0px_3px_8px] rounded-lg'>
+    <div className=' w-5/6 md:w-3/6 lg:w-1/4 p-5 shadow-[rgba(0,0,0,0.2)_0px_3px_8px] rounded-lg'>
         <img className=' rounded-xl h-48 w-full' src={props.img} alt="food"/>
         <div className=' space-y-4'>
             <h1 className=' font-semibold text-center textxl pt-6'>{props.title}</h1>
